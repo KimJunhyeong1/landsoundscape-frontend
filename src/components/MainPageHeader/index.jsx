262a632601fd456435d7fa7a08a0ff7dba637270
junbro <khm11904@gmail.com>
2022-07-04 21:55:20 +0900

    feat: 메인페에지 랜덤하게 사진출력 및 헤더, 하단 버튼 기능 구현

---
+++ src/components/MainPageHeader/index.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { ImLocation2 } from "react-icons/im";
+
+function MainPageHeader({ city, country }) {
+  return (
+    <HeaderWrapper>
+      <HeaderContent>ANDRESS PHOTOS</HeaderContent>
+      <Location>
+        <LocationIcon />
+        <HeaderContent>
+          {city} - {country}
+        </HeaderContent>
+      </Location>
+    </HeaderWrapper>
+  );
+}
+
+export default MainPageHeader;
+
+MainPageHeader.propTypes = {
+  city: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+};
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  width: 99%;
+  padding: 0 0.1rem;
+  position: absolute;
+  top: 0;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Location = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LocationIcon = styled(ImLocation2)`
+  font-size: 2rem;
+  color: white;
+`;
+
+const HeaderContent = styled.h2`
+  color: white;
+`;
