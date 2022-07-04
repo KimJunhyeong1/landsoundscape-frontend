65e0331cec34eda397fbb98378a0531192c4432e
junbro <khm11904@gmail.com>
2022-07-04 21:57:04 +0900

    feat: 반응형 웹 대응을 위한 미디어쿼리 사용

--- src/components/themes/GlobalStyle.jsx
+++ src/components/themes/GlobalStyle.jsx
@@ -1,8 +1,21 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body{
+  html {
+    @media only screen and (max-width: 767px) {
+      font-size: 12px;
+    }
+    @media only screen and (min-width: 768px) and (max-width: 1199px) {
+      font-size: 14px;
+    }
+    @media only screen and (min-width: 1200px){
+      font-size: 16px;
+    }
+  }
+  body {
     margin: 0;
+    overflow-x: hidden;
+    overflow-y: hidden;
   }
 `;
 
