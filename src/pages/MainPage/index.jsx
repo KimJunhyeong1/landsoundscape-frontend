c501defc7d1f3d2828b13fb9ee1c41cab90299e3
junbro <khm11904@gmail.com>
2022-07-06 16:19:10 +0900

    Feat: 추가 북마크 관련 기능, 유저 북마크 리스트 가져오는 쿼리

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,30 +1,57 @@
 import styled from "styled-components";
 import { useEffect, useState } from "react";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import ObjectID from "bson-objectid";
+import { useRecoilValue } from "recoil";
 
 import BottomButtons from "../../components/BottomButtons";
 import AsideButtons from "../../components/AsideButtons";
-import { getRandomPhoto } from "../../api";
+import { getRandomPhotoAndBookmarks, insertBookmark } from "../../api";
 import MainPageHeader from "../../components/MainPageHeader";
+import loginState from "../../recoil/auth";
 
 function MainPage() {
   const randomObjectId = new ObjectID().toHexString();
+  const userData = useRecoilValue(loginState);
   const [currentPhotoId, setCurrentPhotoId] = useState(randomObjectId);
-
-  const { data, refetch } = useQuery(["randomPhoto"], () =>
-    getRandomPhoto(currentPhotoId),
+  const bookmarkMutation = useMutation(insertBookmark);
+  const [isBookmark, setIsBookmark] = useState(false);
+  const { data, refetch } = useQuery(
+    ["randomPhoto"],
+    () => getRandomPhotoAndBookmarks(currentPhotoId, userData?._id),
+    {
+      refetchOnWindowFocus: false,
+    },
   );
 
+  const handleBookmarkButtonClick = () => {
+    bookmarkMutation.mutate(
+      { userId: userData?._id, photoId: currentPhotoId },
+      {
+        onSuccess: payload => {
+          setIsBookmark(payload.bookmarks.some(id => id === data.photo._id));
+        },
+      },
+    );
+  };
+
   useEffect(() => {
-    setCurrentPhotoId(data._id);
+    setIsBookmark(
+      data.user?.bookmarks.some(element => element._id === data.photo._id),
+    );
+    setCurrentPhotoId(data.photo._id);
   }, [data]);
 
   return (
     <PhotoWrapper>
-      <Photo src={data.imageUrl} />
-      <MainPageHeader city={data.city} country={data.country} />
-      <BottomButtons onNewButtonClick={refetch} soundUrl={data.soundUrl} />
+      <Photo src={data.photo.imageUrl} />
+      <MainPageHeader city={data.photo.city} country={data.photo.country} />
+      <BottomButtons
+        onNewButtonClick={refetch}
+        onBookmarkButtonClick={handleBookmarkButtonClick}
+        soundUrl={data.photo.soundUrl}
+        isBookmark={isBookmark}
+      />
       <AsideButtons />
     </PhotoWrapper>
   );
