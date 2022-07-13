34528bc18b6f08db44280938f34b9d4fdd81eb81
junbro <khm11904@gmail.com>
2022-07-13 15:04:01 +0900

    Feat: 업로드 모달 스피너 추가 및 스타일링

--- src/components/Upload/index.jsx
+++ src/components/Upload/index.jsx
@@ -6,6 +6,8 @@ import Select from "react-select";
 import countryList from "react-select-country-list";
 import { countries, getCitiesByCountryCode } from "country-city-location";
 import { useNavigate } from "react-router-dom";
+import { BsCloudUpload } from "react-icons/bs";
+import RingLoader from "react-spinners/RingLoader";
 
 import { fileUpload } from "../../api";
 import loginState from "../../recoil/auth";
@@ -98,31 +100,128 @@ function Upload() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="file"
-        accept="image/jpeg, image/png, image/jpg"
-        onChange={handleImageChange}
-        required
-      />
-      <div>
-        {previewUrl && <ImagePreview src={previewUrl} alt="preview-img" />}
-      </div>
-      <Select
-        options={countryOptions}
-        value={country}
-        onChange={handleCountryChange}
-      />
-      <Select options={cityOptions} value={city} onChange={handelCityChange} />
-      <button type="submit">제출</button>
-    </form>
+    <>
+      {fileUploadMutation.isLoading ? (
+        <SpinnersBox>
+          <RingLoader size={150} color="#265d6e" />
+          <SpinnerContent>Selecting a sound and tags...</SpinnerContent>
+        </SpinnersBox>
+      ) : (
+        <UploadForm onSubmit={handleSubmit}>
+          <label htmlFor="inputFile">
+            {!previewUrl ? (
+              <UploadBox>
+                <UploadIcon />
+                <UploadContent>to upload your photo</UploadContent>
+              </UploadBox>
+            ) : (
+              <ImagePreview src={previewUrl} alt="preview-img" />
+            )}
+          </label>
+          <input
+            id="inputFile"
+            type="file"
+            accept="image/jpeg, image/png, image/jpg"
+            onChange={handleImageChange}
+            required
+            hidden
+          />
+          <CountryCityBox>
+            <ColumnBox>
+              <label htmlFor="countrySelect">COUNTRY</label>
+              <Select
+                id="countrySelect"
+                options={countryOptions}
+                value={country}
+                onChange={handleCountryChange}
+              />
+            </ColumnBox>
+            <ColumnBox>
+              <label htmlFor="citySelect">CITY</label>
+              <Select
+                id="citySelect"
+                options={cityOptions}
+                value={city}
+                onChange={handelCityChange}
+              />
+            </ColumnBox>
+          </CountryCityBox>
+          <SubmitButton type="submit">Submit</SubmitButton>
+        </UploadForm>
+      )}
+    </>
   );
 }
 
+const UploadForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 const ImagePreview = styled.img`
-  width: 20rem;
-  height: 20rem;
+  margin-top: 2rem;
+  width: 25rem;
+  height: 18rem;
   object-fit: cover;
+`;
+
+const UploadBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: 2rem;
+  width: 25rem;
+  height: 18rem;
+  border: 0.16rem dashed #265d6e;
+  background-color: #f8f8ff;
+`;
+
+const UploadIcon = styled(BsCloudUpload)`
+  color: #265d6e;
+  font-size: 3rem;
+`;
+
+const UploadContent = styled.span`
+  color: #265d6e;
+  font-size: 2rem;
+`;
+
+const CountryCityBox = styled.div`
+  width: 90%;
+  margin-top: 1.5rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ColumnBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const SubmitButton = styled.button`
+  margin-top: 2rem;
+  width: 8rem;
+  height: 2.5rem;
+  color: white;
+  background-color: #265d6e;
+  border-radius: 15px;
+  border: none;
+`;
+
+const SpinnersBox = styled.div`
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SpinnerContent = styled.span`
+  margin-top: 2rem;
+  color: #265d6e;
+  font-size: 2rem;
 `;
 
 export default Upload;
